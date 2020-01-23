34f56c34ea2ae0b477830aea35ae0baf89dd98e8
H-THO <hywel.p.thomas@gmail.com>
2020-01-24 10:09:31 +1300

    updated seeds with song file and title

--- seeds/populate-songs.js
+++ seeds/populate-songs.js
@@ -4,11 +4,11 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('songs').insert([
-        { title: '', filepath: './1.mp3', artist: '' },
-        { title: '', filepath: './2.mp3', artist: '' },
-        { title: '', filepath: './3.mp3', artist: '' },
-        { title: '', filepath: './4.mp3', artist: '' },
-        { title: '', filepath: './5.mp3', artist: '' },
+        { title: 'my heart will go on', filepath: './1.mp3', artist: 'celine dion' },
+        { title: 'beat it', filepath: './2.mp3', artist: 'michael jackson' },
+        { title: 'crazy frog', filepath: './3.mp3', artist: 'axel f' },
+        { title: 'i want it that way', filepath: './4.mp3', artist: 'backstreet boys' },
+        { title: 'x gonna give it to ya', filepath: './5.mp3', artist: 'dmx' },
         { title: '', filepath: './6.mp3', artist: '' },
         { title: '', filepath: './7.mp3', artist: '' },
         { title: '', filepath: './8.mp3', artist: '' },
