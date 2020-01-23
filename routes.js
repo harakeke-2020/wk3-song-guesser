521c4ff9c3957ea13a1c03a8f183533f9350a9c6
EDA <james.panaho@gmail.com>
2020-01-24 11:49:23 +1300

    more styling

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const db = require('./db')
 
 router.get('/', (req, res) => {
-  res.render('partials/home')
+  res.render('partials/song')
 })
 
 module.exports = router
